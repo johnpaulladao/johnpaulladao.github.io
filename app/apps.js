91ac0fccc28f16f607d29fb92b4eaec89621fc0e
johnpaulladao <johnpaulladao106@gmail.com>
2023-01-16 21:19:08 +0800

    food galery review footer

--- app/apps.js
+++ app/apps.js
@@ -8,10 +8,15 @@ menu.onclick = () =>{
 
 }
 
-window.onscroll = ()  => {
+window.onscroll = () =>{
 
-    menu.classList.remove ('fa-times');
-    navbar.classList.remove('active');
+  menu.classList.remove('fa-times');
+  navbar.classList.remove('active');
+
+  if(window.scrollY > 60){
+    document.querySelector('#scroll-top').classList.add('active');
+  }else{
+    document.querySelector('#scroll-top').classList.remove('active');
+  }
+
 }
-
- 
